c134edc9bb4d4716c76d18f3502f24568049525c
Saulo Antunes <sauloantuness@gmail.com>
2017-01-27 17:49:21 +0000

    Change fix_municipalities to increase performance

--- scripts/redshift/rais.py
+++ scripts/redshift/rais.py
@@ -76,54 +76,76 @@ class Rais():
         print '+ year'
 
     def fix_municipality(self, location):
-        municipalities = {}
+        municipalities_6 = []
+        municipalities_7 = []
 
         for _, row in location.municipalities_df.iterrows():
                 id_ibge = row['municipality'][:-1]
-                municipalities[id_ibge] = add_dv_to_id_ibge(id_ibge)
+                municipalities_6.append(id_ibge)
+                municipalities_7.append(self.add_dv_to_id_ibge(id_ibge))
 
-        self.df['municipality'].replace(municipalities, inplace=True)
+        municipalities_df = pd.DataFrame({
+            'municipality': pd.Series(municipalities_6),
+            'municipality7': pd.Series(municipalities_7)
+        })
+
+        self.df = pd.merge(
+            self.df, 
+            municipalities_df,
+            on='municipality',
+            how='left'
+        )
+
+        self.df.drop('municipality', 1, inplace=True)
+        self.df.rename(
+            columns={
+                'municipality7': 'municipality'
+            }, 
+            inplace=True
+        )
+
+        print '- fix municipalities'
 
 
-def add_dv_to_id_ibge(id_ibge):
-    invalid = {
-        '220191': '2201919',
-        '220225': '2202251',
-        '220198': '2201988',
-        '261153': '2611533',
-        '311783': '3117836',
-        '315213': '3152131',
-        '430587': '4305871',
-        '520393': '5203939',
-        '520396': '5203962'
-    }
-    
-    if invalid.get(id_ibge):
-        return invalid.get(id_ibge)
+    def add_dv_to_id_ibge(self, id_ibge):
+        invalid = {
+            '220191': '2201919',
+            '220225': '2202251',
+            '220198': '2201988',
+            '261153': '2611533',
+            '311783': '3117836',
+            '315213': '3152131',
+            '430587': '4305871',
+            '520393': '5203939',
+            '520396': '5203962'
+        }
+        
+        if invalid.get(id_ibge):
+            return invalid.get(id_ibge)
 
-    pesos = [1,2,1,2,1,2]
-    ponderacao = []
+        pesos = [1,2,1,2,1,2]
+        ponderacao = []
 
-    for i in range(6):
-        ponderacao.append(int(id_ibge[i]) * pesos[i])
+        for i in range(6):
+            ponderacao.append(int(id_ibge[i]) * pesos[i])
 
-    ponderacao = map(lambda x: x if x < 10 else 1 + (x % 10), ponderacao)
+        ponderacao = map(lambda x: x if x < 10 else 1 + (x % 10), ponderacao)
 
-    soma = reduce(lambda x, y: x + y, ponderacao)
+        soma = reduce(lambda x, y: x + y, ponderacao)
 
-    resto = soma % 10
+        resto = soma % 10
 
-    if resto:
-        dv = 10 - resto
-    else:
-        dv = 0
+        if resto:
+            dv = 10 - resto
+        else:
+            dv = 0
 
-    return id_ibge + str(dv)
+        return id_ibge + str(dv)
 
 
 @click.command()
-@click.argument('input', default='redshift/teste/input/rais', type=click.Path())
-@click.argument('output', default='redshift/teste/output/rais', type=click.Path())
+@click.argument('input', default='redshift/raw_from_mysql/rais', type=click.Path())
+@click.argument('output', default='redshift/raw_from_mysql/rais_formatted', type=click.Path())
 def main(input, output):
     s3 = S3()
     location = Location(s3)
