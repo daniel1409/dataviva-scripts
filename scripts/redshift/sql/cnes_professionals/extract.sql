3f44670d8602820575b742fc3a18cf150aea0a34
Saulo Antunes <sauloantuness@gmail.com>
2017-04-11 15:11:50 -0300

    Add hierarchy level to cnes professional

--- scripts/redshift/sql/cnes_professionals/extract.sql
+++ scripts/redshift/sql/cnes_professionals/extract.sql
@@ -1,3 +1,3 @@
 time mysql -h<host> -u<user> -p<password> <database> 
---batch -e "select cnes, tp_unid, cbo, cns_prof, vinculac, prof_sus, horaoutr, horahosp, hora_amb, competen1, regsaude from PROF_2008_STEP3" | 
+--batch -e "select cnes, codmun, tp_unid, cbo, cns_prof, vinculac, prof_sus, horaoutr, horahosp, hora_amb, competen1, regsaude, niv_hier_2 from PROF_2008_STEP3" | 
 sed 's/\t/","/g;s/^/"/;s/$/"/;s/\n//g' > cnes_professional_2008.csv &&
